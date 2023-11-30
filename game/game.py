b90a2de9b9f0e593f192efb7396a829e4227da02
NicoNunezSahr <nunez.nicolas123@gmail.com>
2023-11-29 16:07:42 -0800

    fiux calls

--- game/game.py
+++ game/game.py
@@ -13,7 +13,7 @@ from datetime import datetime
 import csv
 
 # Set global variables
-NUM_HUMANS = 0
+NUM_HUMANS = 1
 NUM_BOTS = 3
 NUM_PLAYERS = NUM_HUMANS + NUM_BOTS
 MAX_DICE_PER_PLAYER = 5
@@ -111,7 +111,7 @@ class Player:
       conditional_number_probs_st = conditional_number_probs
       conditional_distributions_list_of_lists = []
       for number in range(1, DICE_SIDES + 1):
-        if self.verbose:
+        if self.verbose > 1:
           print(f'{self.playerID}: d = {number}: calls for / calls against = {cumulative_calls_for} / {cumulative_calls_against} -> prob = {conditional_number_prob}')
 
         # These are the unconditional probabilities of the unseen dice
@@ -231,7 +231,7 @@ class Player:
     if decide_bluff == 1:
       bluff_prob, b_idx = self._get_bluff_call(cda_mat)
       if b_idx:
-        if self.verbose:
+        if self.verbose > 1:
           print(f'Bluff: Probability of at least {b_idx[1]+1} {b_idx[0]+1}\'s: {bluff_prob}')
         bluff_action = {'quantity': b_idx[1]+1, 'dice': b_idx[0]+1, 'bs': False, 'exactly': False}
         return bluff_action
@@ -241,7 +241,7 @@ class Player:
     if max_prob <= 0:  # If highest probability play is 0.0, must call bullshit
       return {'quantity': q, 'dice': d, 'bs': True, 'exactly': False}
     else:
-      if self.verbose:
+      if self.verbose > 1:
           print(f'Probability of at least {idx[1]+1} {idx[0]+1}\'s: {max_prob}')
       best_action = {'quantity': idx[1]+1, 'dice': idx[0]+1, 'bs': False, 'exactly': False}
 
@@ -253,16 +253,16 @@ class Player:
     prob_prev_action = self.conditional_dist[d - 1][q]
     prob_bullshit_wins = 1 - prob_prev_action
     if prob_prev_action < self.risk_thres:
-      if self.verbose:
+      if self.verbose > 1:
         print(f'Probability of at least {q} {d}\'s = {prob_prev_action} < {self.risk_thres} -> BS')
       return {'bs': True, 'dice':d, 'quantity':q, 'exactly': False}
-    if self.verbose:
+    if self.verbose > 1:
       print(f'Probability of at least {q} {d}\'s = {prob_prev_action} > {self.risk_thres} -> RAISE')
 
     # Exactly only an option if number of dice left is at least half of what you started with
     # Call exactly if probability is above threshold, or if its more likely than best action
     if self.num_dice_in_game >= (MAX_TOTAL_DICE / 2):
-      if self.verbose:
+      if self.verbose > 1:
         print(f'Probability of exactly {q} {d}\'s: {exa_prob}')
       if exa_prob > self.exactly_thres or (exa_prob > max_prob):
         return {'quantity': q, 'dice': d, 'bs': False, 'exactly': True}
@@ -295,11 +295,13 @@ class HumanPlayer(Player):
         if prev_action is not None:
           if player_call == 'bs':  # BS
             invalid_call = False
-            action = prev_action.update({'bs': True})
+            action = prev_action
+            action.update({'bs': True})
           elif player_call == 'exactly':  # Exactly
             if self.num_dice_unseen + self.hand.size >= MAX_TOTAL_DICE / 2:
                 invalid_call = False
-                action = prev_action.update({'exactly': True})
+                action = prev_action
+                action.update({'exactly': True})
             else:
                 invalid_reason = 'Not enough dice for exactly.'
           elif len([int(i) for i in player_call.split() if i.isdigit()]) == 2:  # RAISE
@@ -314,6 +316,10 @@ class HumanPlayer(Player):
               else:
                 invalid_reason = f'After {prev_action["quantity"]} aces, lowest call is \
                   {prev_action["quantity"] + 1} aces or {2*prev_action["quantity"] + 1} 2s'
+              if invalid_call:
+                  print('ERROR: INVALID CALL. {}. Got: {}'.format(invalid_reason, raise_call))
+              else:
+                  action = {'quantity': quantity, 'dice': dice, 'bs': False, 'exactly': False}
             elif 2 <= prev_action['dice'] <= 6:
               if dice == 1 and quantity >= prev_action['quantity'] / 2:
                 invalid_call = False
@@ -331,10 +337,6 @@ class HumanPlayer(Player):
                     {int(np.ceil(prev_action["quantity"] / 2))} 1s'
           else:
             invalid_reason = f'What is that? Try again.'
-          if invalid_call:
-            print('ERROR: INVALID CALL. {}. Got: {}'.format(invalid_reason, raise_call))
-          else:
-            action = {'quantity': quantity, 'dice': dice, 'bs': False, 'exactly': False}
         else:
           if len([int(i) for i in player_call.split() if i.isdigit()]) == 2:  # RAISE
             raise_call = [int(i) for i in player_call.split() if i.isdigit()]
@@ -346,6 +348,10 @@ class HumanPlayer(Player):
               invalid_reason = 'Dice must be within 1 and 6.'
           else:
             invalid_reason = 'Must start with 2 integers.'
+          if invalid_call:
+            print('ERROR: INVALID CALL. {}. Got: {}'.format(invalid_reason, raise_call))
+          else:
+              action = {'quantity': quantity, 'dice': dice, 'bs': False, 'exactly': False}
 
     return action
 
@@ -556,11 +562,12 @@ def runGame(verbose: int = 0):
         p = Player(hand = h, risk_thres = r, likely_thres = l, exactly_thres = e, bluff_prob = bp, bluff_thres = bt,
                     trustability = trust, playerID= i + 1, num_dice_unseen = total_dice_left - h.size,
                     verbose = verbose)
+        player_metadata.append([p.playerID, p.risk_thres, p.likely_thres, p.exactly_thres, p.bluff_prob, p.bluff_thres, p.trustability])
 
       elif player_type == 'HUMAN':
         p = HumanPlayer(hand = h, playerID = i + 1, trustability = trust, num_dice_unseen = total_dice_left - h.size)
+        player_metadata.append([p.playerID, -1, -1, -1, -1, -1, -1])  # Dummy data for humans
 
-      player_metadata.append([p.playerID, p.risk_thres, p.likely_thres, p.exactly_thres, p.bluff_prob, p.bluff_thres, p.trustability])
       player_list.append(p)  # Keeps track of players left in game
 
   if verbose:
@@ -613,7 +620,14 @@ def runGame(verbose: int = 0):
         prev_a = a
 
         if verbose:
-          print('turn {}: {}'.format(i, a))
+          #print('turn {}: {}'.format(i, a))
+
+          if a['bs'] == True:
+            print("Turn " + str(i) +  ": BULLSHIT!!!")
+          elif a['exactly'] == True:
+            print("Turn " + str(i) +  ": EXACTLY")
+          else:
+            print("Turn " + str(i) + ": " + str(a['quantity']) + " " + str(a['dice']) + "s")
 
         i += 1
 
@@ -718,7 +732,7 @@ if __name__ == '__main__':
         csvwriter = csv.writer(csvfile, delimiter=',')
         csvwriter.writerow(['player_id', 'risk_thres', 'likely_thres', 'exactly_thres', 'bluff_prob', 'bluff_thres', 'trustability', 'win_bool'])
         for i, games in enumerate(range(max_games)):
-          game_metadata = runGame(verbose=0)
+          game_metadata = runGame(verbose=1)
           gameWin = game_metadata["game_playerid_winner"]
           player_metadata = game_metadata["player_metadata"]
           if i % 10 == 0:
